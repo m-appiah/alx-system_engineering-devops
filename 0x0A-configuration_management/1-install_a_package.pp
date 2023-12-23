96999402b2ea1809aac36e9d3686b0e7bc3ecfbc
Michael Appiah <mappiahca@gmail.com>
2023-12-22 18:24:46 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #Install flask version 2.1.0 from pip3 using Puppet
+#First install werkzeug
+
+package { 'werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug']
 }

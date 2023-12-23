db28f748cc7fab3b83b92597bf9547bcd70071ef
Michael Appiah <mappiahca@gmail.com>
2023-12-22 19:12:05 -0800

    Execute a command bug fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,5 +2,5 @@
 
 exec { 'killmenow_process':
   command  => 'pkill -f killmenow',
-  path     => ['/usr/sbin', '/usr/bin']      
+  path     => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']      
 }

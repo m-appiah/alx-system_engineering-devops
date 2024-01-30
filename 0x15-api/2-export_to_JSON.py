ba1f4ba363f2e33b66c0a42b56faaf999d5c25d0
Michael Appiah <mappiahca@gmail.com>
2024-01-29 20:19:39 -0800

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Using REST API to access todo lists of employees"""
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    employeeId = argv[1]
+    mainUrl = "https://jsonplaceholder.typicode.com/users"
+    url = mainUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    dictionary = {employeeId: []}
+    for task in tasks:
+        dictionary[employeeId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+            })
+        with open('{}.json'.format(employeeId), 'w') as filename:
+            json.dump(dictionary, filename)

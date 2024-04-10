6d03f3e7a682e1faabda3d7e0c68195189de51c9
Michael Appiah <mappiahca@gmail.com>
2024-04-10 04:14:23 +0000

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""shebang line"""
+"""function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.s
+e"""
 
 import requests
 

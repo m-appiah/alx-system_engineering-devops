01bb7686ca0c1068cded00b9bda142c51adf2cf2
Michael Appiah <mappiahca@gmail.com>
2024-04-11 00:47:44 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ def number_of_subscribers(subreddit):
     """
     function that queries the Reddit API and returns the number of subscribers
     """
-    headers = {'User-Agent': 'chrome'}
+    headers = {'User-Agent': 'Chrome'}
     response = requests.get(
-            f'https://www.reddit.com/r/{subreddit}/about.json',
+            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json()['data']['subscribers']
